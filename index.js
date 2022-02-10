745189d86f8f5b48e13fd5bfb1c9c9a796102eb0
alexandermaxwellogle <maxogle@gmail.com>
2022-02-09 22:36:53 -0500

    commit tree 9b0e7c6e9fbb180f83cd4f0209258fe74df4b23d

---
+++ index.js
@@ -0,0 +1,63 @@
+var a, b 
+
+
+
+function add(){
+
+}
+
+
+
+function subtract(){
+
+}
+
+
+
+function divide(){
+
+}
+
+
+
+function multiply(){
+
+}
+
+
+
+function add(a, b) {
+return a + b
+}
+
+
+function subtract(a, b) {
+    return a - b
+}
+
+
+
+function divide(a, b) {
+    return a / b
+}
+
+
+function multiply(a, b) {
+    return a * b
+}
+
+function increment(a) {
+    return a + 1
+}
+
+function decrement(a) {
+    return a - 1
+}
+
+function makeInt(a) {
+    return parseInt(a, 10);
+}
+
+function preserveDecimal(a) {
+    return parseFloat(a);
+}
